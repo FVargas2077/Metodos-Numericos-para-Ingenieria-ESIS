9489e8d1434f9e756dd2b66227a5a100d056701d
Fabian Arturo Vargas Quispe <fvargasqui@unjbg.edu.pe>
2025-05-26 18:05:17 -0500

    Update MN17_Metodo_de_Euler.m

--- MN17_Metodo_de_Euler.m
+++ MN17_Metodo_de_Euler.m
@@ -1,3 +1,4 @@
+% PROGRAMA DEL METODO DE EULER
 clc;
 clear;
 n = input('Ingrese el numero de subintervalos: ');
@@ -7,8 +8,8 @@ xn = input('Ingrese la abcisa donde se desea calcular y: ');
 func_str = input('Ingrese la funcion f(x, y): ', 's');
 f = str2func(['@(x, y)', func_str]);
 H = (xn - x0) / n;
-yi=y0;
-xi=x0;
+yi = y0;
+xi = x0;
 for i = 1:n
     yi = yi + H * f(xi, yi);
     xi = x0 + i * H;
